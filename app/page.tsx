d8f848390d6e061b10e0c51c4e03b7bb1e228f09
synbiokz <50535229+synbiokz@users.noreply.github.com>
2025-01-26 22:40:29 -0500

    Simple test page test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+/*
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">SEC Insider</h1>
       <p>Welcome to the future of insider trading analysis</p>
     </main>
+  )
+}
+*/
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Test Page</h1>
+    </div>
   )
 }
